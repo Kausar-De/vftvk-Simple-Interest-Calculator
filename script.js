334d3743efd4c04e32e6fcd87b782adb65a74c12
Kausar-De <kde48business@gmail.com>
2021-03-22 16:15:11 +0530

    Update Simple-Interest-Calculator
    Changed interest rate to slider
    Changed number of years to dropdown box
    Added copyright message
    Styled the page according to project specifications
    Changed compute() to calculate interest and display requested output
    Added rate() function to show user's selection on rate slider

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseFloat(document.getElementById("years").value);
+    var interest = (principal * years * rate) / 100
+
+    if(document.getElementById("principal").value == "")
+    {
+        alert("Please enter the principal amount");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    else if(document.getElementById("principal").value <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
+    var today = new Date();
+    var currentYear = today.getFullYear() + years; 
+
+    document.getElementById("result1").innerHTML = "If you deposit " + principal + ",";
+    document.getElementById("result2").innerHTML = "at an interest rate of " + rate + "%.";
+    document.getElementById("result3").innerHTML = "You will receive an amount of " + interest + ",";
+    document.getElementById("result4").innerHTML = "in the year " + currentYear;
+
 }
-        
+
+function rate()
+{
+    var ratevalue = document.getElementById("rate");
+    document.getElementById("rateshow").innerHTML = ratevalue.value + "%";
+}
